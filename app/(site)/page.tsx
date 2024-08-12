c96ce305531e278842e785d69da14a7ae8a9be71
open I <1469859091@qq.com>
2024-08-12 20:41:39 +0800

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,7 +4,7 @@ import ListItem from "@/components/ListItem";
 
 import PageContent from "./components/PageContent";
 
-export const ravalidate = 0;
+export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
   return (
